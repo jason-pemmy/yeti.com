ff580f4c19b1e30fe34b7094af852ab1afe66d06
Jason <jason.pemmy@gmail.com>
2017-02-08 08:15:18 -0500

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).on("ready",function(){
-	
+    $(".hamburger-menu").on("click", function(){
+        $(this).toggleClass("open");
+        $(".yeti-mobile-menu").toggleClass("open");
+    });    
 });
