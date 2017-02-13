f3fe30fa6d0b99d7104a314166952b93bd5a5273
Jason Pembleton CW <A6CH1ZZ@usac.mmm.com>
2017-02-13 16:37:20 -0500

    adding content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $(document).on("ready",function(){
     $(".hamburger-menu").on("click", function(){
-        $(this).toggleClass("open");
-        $(".yeti-mobile-menu").toggleClass("open");
+		$(this).toggleClass("open");
+		
+		if(!$(".yeti-mobile-menu").hasClass("open")) {
+			console.log("is closed");
+			$(".yeti-mobile-menu").css("z-index", "1").delay(2000).addClass("open");
+		}else {
+			$(".yeti-mobile-menu").removeClass("open");
+			setTimeout(function(){
+				$(".yeti-mobile-menu").css("z-index", "-1");
+			},600);
+		}
     });    
 });
